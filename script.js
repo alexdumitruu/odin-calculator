165b0f0258b4f53a9f9c97b334f3bca54ce6b525
alexdumitruu <alex.dumitru.g@gmail.com>
2025-08-16 13:25:17 +0300

    finish html and css, to make script

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 
-
-function add(a, b)
-{
-    return a + b;
-}
-
-function subtract(a, b)
-{
-    return a - b;
-}
-
-function multiply(a, b)
-{
-    return a * b;
-}
-
-function divide(a, b)
-{
-    return a / b;
-}
+const add = (a, b) => {
+	return a + b;
+};
+const subtract = (a, b) => {
+	return a - b;
+};
+const multiply = (a, b) => {
+	return a * b;
+};
+const divide = (a, b) => {
+	return a / b;
+};
 
 // TESTING
 
 //console.log(subtract(6, 9))
 
-let firstNumber;
-let secondNumber;
-let operator;
 
 function operate(operand1, operand2, operator)
 {
     add(operand1, operand2);
+}
+
+let firstInteger = "";
+let secondInteger = "";
+let operator = "";
+let result = "";
+
+function populateDisplay()
+{
+
 }
