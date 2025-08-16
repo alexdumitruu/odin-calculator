4d5b5f8d6c0b10bc9bfde35c63d23036c62366f0
alexdumitruu <alex.dumitru.g@gmail.com>
2025-08-16 12:40:49 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+function add(a, b)
+{
+    return a + b;
+}
+
+function subtract(a, b)
+{
+    return a - b;
+}
+
+function multiply(a, b)
+{
+    return a * b;
+}
+
+function divide(a, b)
+{
+    return a / b;
+}
+
+// TESTING
+
+//console.log(subtract(6, 9))
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(operand1, operand2, operator)
+{
+    add(operand1, operand2);
+}
